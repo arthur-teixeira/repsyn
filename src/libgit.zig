24744212569f9abb3870ffe5d9b4b68238864498
arthur.teixeira <arthurcarvalhot@yahoo.com.br>
2025-11-17 16:05:31 -0300

    Finding branch references (does not work for remote)

--- src/libgit.zig
+++ src/libgit.zig
@@ -3,14 +3,104 @@ const libgit = @cImport({
     @cInclude("git2.h");
 });
 
-const LibGitError = error {
-    InitError
+const GitError = error {
+    InitError,
+    AlreadyInitialized,
+    AlreadyShutdown,
+    BranchNotFound,
+    DiffError,
 };
 
-pub fn init() LibGitError!void {
-    const ret = libgit.git_libgit2_init();
-    std.debug.print("Called libgit {d}\n", .{ret});
-    if (ret != 1) {
-        return LibGitError.InitError;
+const Where = enum(c_uint) {
+    None = 0,
+    Local = libgit.GIT_BRANCH_LOCAL,
+    Remote = libgit.GIT_BRANCH_REMOTE,
+};
+
+pub const Repository = struct {
+    repo: ?*libgit.struct_git_repository,
+
+    pub fn init(repository_path: []const u8) GitError!Repository {
+        const ret = libgit.git_libgit2_init();
+        if (ret == 0) {
+            return GitError.InitError;
+        }
+        if (ret > 1) {
+            return GitError.AlreadyInitialized;
+        }
+
+        var repo: ?*libgit.struct_git_repository = null;
+        if (libgit.git_repository_open(&repo, repository_path.ptr) != 0) {
+            return GitError.InitError;
+        }
+
+        return Repository {
+            .repo = repo,
+        };
+    }
+
+    pub fn deinit(r: Repository) GitError!void {
+        errdefer print_error();
+        std.debug.assert(r.repo != null);
+        libgit.git_repository_free(r.repo.?);
+
+        const ret = libgit.git_libgit2_shutdown();
+        if (ret != 0) {
+            return GitError.AlreadyShutdown;
+        }
+    }
+
+    pub fn diff_to_remote(r: Repository, tree1: Tree, tree2: Tree) !void {
+        var diff: ?*libgit.struct_git_diff = null;
+        if (libgit.git_diff_tree_to_tree(&diff, r.repo, tree1.tree, tree2.tree, null) != 0) {
+            return GitError.DiffError;
+        }
+
+        libgit.git_diff_free(diff);
+
+        std.debug.print("Got diff between remote and local \n", .{});
+    }
+
+    pub fn find_branch_tree(repo: Repository, branch_name: []const u8, where: Where) !Tree {
+        errdefer print_error();
+        var branch: ?*libgit.struct_git_reference = null;
+        if (libgit.git_branch_lookup(&branch, repo.repo, branch_name.ptr, @intFromEnum(where)) != 0) {
+            return GitError.BranchNotFound;
+        }
+        defer libgit.git_reference_free(branch);
+
+        var obj: ?*libgit.struct_git_object = null;
+        if (libgit.git_reference_peel(&obj, branch, libgit.GIT_OBJECT_COMMIT) != 0) {
+            return GitError.BranchNotFound;
+        }
+        return Tree.from_commit_object(obj.?);
+    }
+};
+
+pub const Tree = struct {
+    tree: *libgit.struct_git_tree,
+
+    fn from_commit_object(obj: *libgit.struct_git_object) !Tree {
+        defer libgit.git_object_free(obj);
+        const as_commit: *libgit.struct_git_commit = @ptrCast(obj);
+
+        var tree: ?*libgit.struct_git_tree = null;
+        if (libgit.git_commit_tree(&tree, as_commit) != 0) {
+            return GitError.BranchNotFound;
+        }
+        return Tree {
+            .tree = tree.?,
+        };
+    }
+
+    pub fn deinit(tree: Tree) void {
+        libgit.git_tree_free(tree.tree);
+    }
+};
+
+fn print_error() void {
+    const err = libgit.git_error_last();
+    if (err != null) {
+        std.debug.print("ERROR: {s}\n", .{err.*.message});
     }
 }
--- src/main.zig
+++ src/main.zig
@@ -3,25 +3,12 @@ const clap = @import("clap");
 const git = @import("./libgit.zig");
 
 pub fn main() !void {
-    try git.init();
+    const repo: git.Repository = try .init("./");
+    defer repo.deinit() catch { };
+    const local_master = try repo.find_branch_tree("master", .Local);
+    const remote_master = try repo.find_branch_tree("master", .Remote);
+
+    try repo.diff_to_remote(local_master, remote_master);
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-}
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
