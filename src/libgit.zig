9c1b12270ca8b002a96232ed4b0557550d93ac76
arthur.teixeira <arthurcarvalhot@yahoo.com.br>
2025-11-20 15:20:55 -0300

    Collecting branch names

--- src/libgit.zig
+++ src/libgit.zig
@@ -3,24 +3,29 @@ const libgit = @cImport({
     @cInclude("git2.h");
 });
 
-const GitError = error {
+pub const GitError = error {
     InitError,
     AlreadyInitialized,
     AlreadyShutdown,
     BranchNotFound,
     DiffError,
+    FetchError,
 };
 
 const Where = enum(c_uint) {
-    None = 0,
     Local = libgit.GIT_BRANCH_LOCAL,
     Remote = libgit.GIT_BRANCH_REMOTE,
 };
 
 pub const Repository = struct {
+    allocator: std.mem.Allocator,
     repo: ?*libgit.struct_git_repository,
+    remotes: libgit.git_strarray,
+    remote_conns: std.StringHashMap(*libgit.struct_git_remote),
 
-    pub fn init(repository_path: []const u8) GitError!Repository {
+    pub fn init(allocator: std.mem.Allocator, repository_path: []const u8) GitError!Repository {
+        // errdefer print_error();
+
         const ret = libgit.git_libgit2_init();
         if (ret == 0) {
             return GitError.InitError;
@@ -34,52 +39,82 @@ pub const Repository = struct {
             return GitError.InitError;
         }
 
-        return Repository {
+        var remotes: libgit.git_strarray = undefined;
+        if (libgit.git_remote_list(&remotes, repo) != 0) {
+            return GitError.InitError;
+        }
+
+        var r = Repository {
+            .allocator = allocator,
             .repo = repo,
+            .remotes = remotes,
+            .remote_conns = .init(allocator),
         };
+
+        for (0..remotes.count) |i| {
+            const remote_name = remotes.strings[i];
+            const remote_name_str = std.mem.span(remote_name);
+            var remote: ?*libgit.struct_git_remote = null;
+            if (libgit.git_remote_lookup(&remote, repo, remote_name) != 0) {
+                return GitError.InitError;
+            }
+            r.remote_conns.put(remote_name_str, remote.?) catch {
+                return GitError.InitError;
+            };
+        }
+
+        return r;
     }
 
-    pub fn deinit(r: Repository) GitError!void {
-        errdefer print_error();
+    // pub fn fetch(repo: Repository, remote_name: []const u8) !void {
+    //     errdefer print_error();
+    //     const remote = repo.remote_conns.get(remote_name);
+    //     if (remote == null) {
+    //         return GitError.BranchNotFound;
+    //     }
+    //
+    //     libgit.git_remote_connect(remote, libgit.GIT_DIRECTION_FETCH, callbacks: [*c]const struct_git_remote_callbacks, null, null);
+    //
+    //     if (libgit.git_remote_fetch(remote, null, null, null) != 0) {
+    //         return GitError.FetchError;
+    //     }
+    // }
+
+    pub fn find_branch_tree(repo: Repository, branch_name: []const u8, remote_name: []const u8) !Tree {
+        if (remote_name.len == 0) {
+            return git_find_branch_tree(repo.repo, branch_name, .Local);
+        }
+
+        const remote = repo.remote_conns.get(remote_name);
+        if (remote == null) {
+            return GitError.BranchNotFound;
+        }
+
+        return git_find_branch_tree(remote, branch_name, .Remote);
+    }
+
+    pub fn deinit(r: *Repository) GitError!void {
+        // errdefer print_error();
         std.debug.assert(r.repo != null);
         libgit.git_repository_free(r.repo.?);
+        libgit.git_strarray_free(&r.remotes);
+
+        // var it = r.remote_conns.valueIterator();
+        // while (it.next()) |remote| {
+            // libgit.git_repository_free(remote.*);
+        // }
+
+        r.remote_conns.deinit();
 
         const ret = libgit.git_libgit2_shutdown();
         if (ret != 0) {
             return GitError.AlreadyShutdown;
         }
     }
-
-    pub fn diff_to_remote(r: Repository, tree1: Tree, tree2: Tree) !void {
-        var diff: ?*libgit.struct_git_diff = null;
-        if (libgit.git_diff_tree_to_tree(&diff, r.repo, tree1.tree, tree2.tree, null) != 0) {
-            return GitError.DiffError;
-        }
-
-        libgit.git_diff_free(diff);
-
-        std.debug.print("Got diff between remote and local \n", .{});
-    }
-
-    pub fn find_branch_tree(repo: Repository, branch_name: []const u8, where: Where) !Tree {
-        errdefer print_error();
-        var branch: ?*libgit.struct_git_reference = null;
-        if (libgit.git_branch_lookup(&branch, repo.repo, branch_name.ptr, @intFromEnum(where)) != 0) {
-            return GitError.BranchNotFound;
-        }
-        defer libgit.git_reference_free(branch);
-
-        var obj: ?*libgit.struct_git_object = null;
-        if (libgit.git_reference_peel(&obj, branch, libgit.GIT_OBJECT_COMMIT) != 0) {
-            return GitError.BranchNotFound;
-        }
-        return Tree.from_commit_object(obj.?);
-    }
 };
 
 pub const Tree = struct {
     tree: *libgit.struct_git_tree,
-
     fn from_commit_object(obj: *libgit.struct_git_object) !Tree {
         defer libgit.git_object_free(obj);
         const as_commit: *libgit.struct_git_commit = @ptrCast(obj);
@@ -92,15 +127,25 @@ pub const Tree = struct {
             .tree = tree.?,
         };
     }
+ };
 
-    pub fn deinit(tree: Tree) void {
-        libgit.git_tree_free(tree.tree);
-    }
-};
+pub fn deinit(tree: Tree) void {
+    libgit.git_tree_free(tree.tree);
+}
 
-fn print_error() void {
-    const err = libgit.git_error_last();
-    if (err != null) {
-        std.debug.print("ERROR: {s}\n", .{err.*.message});
+
+fn git_find_branch_tree(repo: ?*libgit.struct_git_repository, branch_name: []const u8, where: Where) !Tree {
+    // errdefer print_error();
+    var branch: ?*libgit.struct_git_reference = null;
+    if (libgit.git_branch_lookup(&branch, repo, branch_name.ptr, @intFromEnum(where)) != 0) {
+        return GitError.BranchNotFound;
     }
+    defer libgit.git_reference_free(branch);
+
+    var obj: ?*libgit.struct_git_object = null;
+    if (libgit.git_reference_peel(&obj, branch, libgit.GIT_OBJECT_COMMIT) != 0) {
+        return GitError.BranchNotFound;
+    }
+
+    return Tree.from_commit_object(obj.?);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,104 @@
 const std = @import("std");
 const clap = @import("clap");
 const git = @import("./libgit.zig");
+const GitError = git.GitError;
+const StrArray = @import("./strarray.zig").StrArray;
+const libgit = @cImport({
+    @cInclude("git2.h");
+});
 
 pub fn main() !void {
-    const repo: git.Repository = try .init("./");
-    defer repo.deinit() catch { };
-    const local_master = try repo.find_branch_tree("master", .Local);
-    const remote_master = try repo.find_branch_tree("master", .Remote);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    // const allocator = gpa.allocator();
+    const repository_path = "./";
 
-    try repo.diff_to_remote(local_master, remote_master);
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    errdefer print_error();
 
+    var ret = libgit.git_libgit2_init();
+    if (ret == 0) {
+        return GitError.InitError;
+    }
+    if (ret > 1) {
+        return GitError.AlreadyInitialized;
+    }
+
+    var repo: ?*libgit.struct_git_repository = null;
+    ret = libgit.git_repository_open(&repo, repository_path.ptr);
+    if (ret != 0) {
+        return GitError.InitError;
+    }
+
+    var remotes: libgit.git_strarray = undefined;
+    ret = libgit.git_remote_list(&remotes, repo);
+    if (ret != 0) {
+        return GitError.InitError;
+    }
+
+    std.debug.assert(remotes.count > 0);
+
+    const remote_name = remotes.strings[0];
+    var remote: ?*libgit.struct_git_remote = null;
+    ret = libgit.git_remote_lookup(&remote, repo, remote_name);
+    if (ret != 0) {
+        return GitError.InitError;
+    }
+    defer libgit.git_remote_free(remote);
+
+    var cbs: libgit.struct_git_remote_callbacks = undefined;
+    ret = libgit.git_remote_init_callbacks(&cbs, libgit.GIT_REMOTE_CALLBACKS_VERSION);
+    if (ret != 0) {
+        return GitError.InitError;
+    }
+
+    ret = libgit.git_remote_connect(remote, libgit.GIT_DIRECTION_PUSH, &cbs, null, null);
+    if (ret != 0) {
+        return GitError.InitError;
+    }
+
+    var opts: libgit.git_push_options = undefined;
+    ret = libgit.git_push_options_init(&opts, libgit.GIT_PUSH_OPTIONS_VERSION);
+    if (ret != 0) {
+        return GitError.InitError;
+    }
+
+    var iter: ?*libgit.git_branch_iterator = null;
+    ret = libgit.git_branch_iterator_new(&iter, repo, libgit.GIT_BRANCH_LOCAL);
+    if (ret != 0) {
+        return GitError.InitError;
+    }
+
+    // const refs_arr = try StrArray.init(allocator, push_refs);
+    // defer refs_arr.deinit();
+
+    while(true) {
+        var branch_ref: ?*libgit.git_reference = null;
+        var branch_type: libgit.git_branch_t = 0;
+        ret = libgit.git_branch_next(&branch_ref, &branch_type, iter);
+        if (ret == libgit.GIT_ITEROVER) {
+            break;
+        }
+        if (ret != 0) {
+            return GitError.InitError;
+        }
+        defer libgit.git_reference_free(branch_ref);
+
+        const branch_ref_name = libgit.git_reference_name(branch_ref);
+        std.debug.print("{s} is a branch \n", .{branch_ref_name});
+    }
+
+    //
+    // const as_lg_array = refs_arr.libgit_view();
+    // ret = libgit.git_remote_push(remote, @ptrCast(&as_lg_array), &opts);
+    // if (ret != 0) {
+    //     return GitError.InitError;
+    // }
+}
+
+
+fn print_error() void {
+    const err = libgit.git_error_last();
+    if (err != null) {
+        std.debug.print("ERROR: {s}\n", .{err.*.message});
+    }
 }
